613783672ac6baa8fefe03c56de1299fde3ab676
ducmi <125350641+ducmi@users.noreply.github.com>
2023-02-19 01:54:37 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+#first_name = "dep"
+#last_name = "trai"
+#full_name = first_name +" "+ last_name
+#print ("hello "+(full_name))
+#print (type(full_name))
+
+#age = 21
+#age += 1
+#print (age)
+#print ("your age is: "+str(age))
+#print (type(age))
+
+height = 190.5
+print ('your height is: '+(height)+"cm")
+#print (type(height))
+
