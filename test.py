d3e5355f50fcc807a72bd8eae54c2c029ecea379
ducmi <125350641+ducmi@users.noreply.github.com>
2023-02-22 16:31:38 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -10,7 +10,41 @@
 #print ("your age is: "+str(age))
 #print (type(age))
 
-height = 190.5
-print ('your height is: '+(height)+"cm")
+#height = 190.5
+#print ('your height is: '+str(height)+"cm")
 #print (type(height))
 
+#human = False
+#print("Are you a human: "+str(human))
+#print(type(human))
+
+#MULTIPLE ASSIGNMENT:-----------------------------------------
+#name = "bro"
+#age = 21
+#attractive = True
+
+#name, age, attractive = "Bro", 21, True
+
+#print (name)
+#print (age)
+#print (attractive)
+
+#saitama = garou = boros = 30
+
+#print (saitama)
+#print (garou)
+#print (boros)
+
+#STRING METHOD
+#name = "Bro Code"
+
+#print (len(name))
+#print(name.find("o"))  #tim thu tu chu trong dong nhat dinh
+#print (name.capitalize())   
+#print (name.upper())   #dau in hoa
+#print (name.lower())   #dau thuong
+#print (name.isdigit())  #check that is the number or not
+#print (name.isalpha())  #check if the string is only alpha bet letter
+#print (name.count("o"))
+#print (name.replace("o","a"))   #replace the lettere you want to another letter
+#print (name*3) #x3 print
