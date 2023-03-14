9cd161386c6a4cb190aafc1b275766b327893576
ducmi <125350641+ducmi@users.noreply.github.com>
2023-03-14 18:23:23 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -158,3 +158,106 @@
 #        print (symbol, end="")
 #    print(symbol)
 
+#LOOP CONTROL STATEMENT (ko lam theo y muon thi loop tiep)
+
+#break=      used to terminate the loop entirely
+#continue=   skip to the next iteration of the loop
+#pass=       does nothing, act as a place holder
+
+#while True:
+#    name = input("Enter your name: ")
+#    if name != "":
+#        break
+
+#phone_number = "123-456-7890"
+
+#for i in phone_number:
+#    if i == "-":
+#        continue
+#    print(i, end="")
+
+#for i in range(1,21):
+
+#    if i == 13:
+#        pass
+#    else:
+#        print (i)
+
+#LIST = used to store multiple items in a single variable
+
+#food = ["pizza","hambergur","hotdog","spaghetti","pudding"]
+
+#food[0] = "sushi"
+#print (food[3])
+#food.append("ice cream")   (add in)
+#food.remove("hotdog")      (remove)
+#food.pop()                 (remove)
+#food.sort()                (sort a->z theo thu tu)
+#food.clear()               (xoa het trong food)
+
+#for x in food:
+#    print(x)
+
+#2D LIST: A LIST OF LIST
+
+#drinks = ["coffe","soda","tea"]
+#dinner = ["pizza","hamburger","hotdog"]
+#dessert = ["cake","ice cream"]
+
+#food = [drinks ,dinner ,dessert]
+
+#print(food[0][0])
+
+#TUPLE = COLLECTION WHICH IS ORDERED AND UNCHANGEABLE
+#           USED TO GROUP TOGETHER RELATED DATA
+
+#student = ("Bro",21,'male')
+
+#print(student.count("Bro"))
+#print(student.index("male"))
+
+#for x in student:
+#    print (x)
+
+#if "Bro" in student:
+#    print("Bro is here!")
+
+#set = collection which is unordered, unindexed. No duplicate value
+
+#utensils = {"fork","spoon","knife"}
+#dishes = {"bowl","plate", "cup","knife"}
+#for x in utensils:
+#    print (x)
+
+#utensils.add("napkin")
+#utensils.remove("fork")
+#utensils.clear ()
+#dishes.update (utensils)
+#dinner_table = utensils.union(dishes)
+
+#for x in dishes:
+#    print (x)
+
+#print(utensils.difference(dishes))
+#print(utensils.intersection(dishes)) 
+
+#DICTIONARY:    A CHANGEABLE, UNORDERED COLLECTION OF UNIQUE KEY: VALUE PAIRS
+#                FAST BECAUSE THEY USE HASHING, ALLOW US TO ACCESS A VALUE QUICKLY
+
+capitals = {'USA':'Washington DC',
+            'India':'New Dehli',
+            'China':'Beijing',
+            'Russia':'Moscow'}
+
+capitals.update({'Germany','Berlin'})
+capitals.update({'USA':'Las Vegas'})
+capitals.pop('China')
+capitals.clear()
+#print(capitals['Russia'])
+#print(capitals.get('Germany'))
+#print(capitals.keys())
+#print(capitals.values())
+#print(capitals.items())
+
+for key,value in capitals.items():
+    print(key, value)
